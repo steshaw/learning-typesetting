4d1e35e1cf9d57ffc03249de2aa8d92ff0088ba2
Steven Shaw <steven@steshaw.org>
2019-11-26 19:49:55 +1000

    XeLaTeX example

---
+++ latex/xelatex-example/shell.nix
@@ -0,0 +1,13 @@
+{ pkgs ? import <nixpkgs> {} }:
+with pkgs;
+mkShell {
+  buildInputs = [
+    (
+      texlive.combine {
+        inherit (texlive)
+          scheme-small
+          ;
+      }
+    )
+  ];
+}
